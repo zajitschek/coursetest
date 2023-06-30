87f853a7c5ae1064847e2b6f10adce4233a5a381
Felix Zajitschek <64367116+zajitschek@users.noreply.github.com>
2023-06-30 12:20:11 +0100

    Update install.R
    include older lme4 source version from cran

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,2 @@
-install.packages("tidyverse")
-install.packages("rjson")
+path <- "[http://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_0.7.0.tar.gz](http://cran.r-project.org/src/contrib/Archive/lme4/lme4_1.1-21.tar.gz)"
+install.packages(path, repos = NULL, type = "source")
